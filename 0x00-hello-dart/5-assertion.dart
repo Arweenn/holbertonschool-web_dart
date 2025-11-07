d3bea28f968f626dfd5e7c7e42a0119696a0393c
Arweenn <arwendumont25@outlook.fr>
2025-11-07 15:10:00 +0100

    MAJ OUPS

--- 0x00-hello-dart/5-assertion.dart
+++ 0x00-hello-dart/5-assertion.dart
@@ -4,7 +4,7 @@ void main(List<String> args) {
   final int? n = int.tryParse(args[0]);
   assert(n != null, 'Invalid number');
   assert(n! >= 80, 'The score must be greater than 80');
-  print('You passed');
+  print('You Passed');
 }
 
 
