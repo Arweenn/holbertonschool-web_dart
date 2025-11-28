9a55d70473eb0ef13440a556804b84c943954947
Arweenn <arwendumont25@outlook.fr>
2025-11-28 11:06:05 +0100

    OUTPUT

--- 0x02-dart-classes/2-password.dart
+++ 0x02-dart-classes/2-password.dart
@@ -5,11 +5,11 @@ class Password {
     if (!(password.length >= 8 && password.length <= 16)) return false;
     if (!(password.contains(RegExp(r'[A-Z]')))) return false;
     if (!(password.contains(RegExp(r'[a-z]')))) return false;
-    if (!(password.contains(RegExp(r'\d]')))) return false;
+    if (!(password.contains(RegExp(r'\d')))) return false;
     return true;
   }
     @override
   String toString() {
-    return 'Your password is $password';
+    return 'Your Password is: $password';
   }
 }
