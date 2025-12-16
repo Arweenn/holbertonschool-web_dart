1104066501b70d1170d7996d1a6e62b7c773a1d7
Arweenn <arwendumont25@outlook.fr>
2025-12-16 10:13:43 +0100

    task 4

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,21 @@
+import '4-util.dart';
+import 'dart:convert';
+
+// Function that calculates the total price of a user's orders
+Future<dynamic> calculateTotal() async {
+  try {
+    Map userData = jsonDecode(await fetchUserData());
+    String userId = userData["id"];
+    List userOrders = jsonDecode(await fetchUserOrders(userId));
+    double total = 0;
+
+    for (final product in userOrders) {
+      double price = double.parse(await fetchProductPrice(product));
+      total += price;
+    }
+
+    return total;
+  } catch (error) {
+    return -1;
+  }
+}
